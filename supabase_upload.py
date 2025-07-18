2033e2776a4f086d8f994d47ab8aef6537ee5d17
cpbhamu615 <130212315+cpbhamu615@users.noreply.github.com>
2025-07-18 14:43:45 +0530

    add

--- supabase_upload.py
+++ supabase_upload.py
@@ -2,7 +2,7 @@ from supabase import create_client, Client
 
 # Supabase Credentials
 url = "https://qtpwefwbcncbdgrivzla.supabase.co"    # Your Supabase URL
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."     # Your Supabase Anon Key
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0cHdlZndiY25jYmRncml2emxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4MjIzMDAsImV4cCI6MjA2ODM5ODMwMH0.GAv0sn3NFUZ7CFbF3j2BmGoU8P8FlQzdHUrX5iExlbo"     # Your Supabase Anon Key
 
 supabase: Client = create_client(url, key)
 
